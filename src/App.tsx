41a14e71a1aaecb55f35f4df867e90242d5095db
Karolina <karola.rzucidlo14@gmail.com>
2026-09-21 21:40:15 +0200

    feat: add home section

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Pricing from "./pages/Pricing";
 import Promotions from "./pages/Promotions";
 import Services from "./pages/Services";
--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div>
-      <h1>Strona główna</h1>
-    </div>
-  );
-}
-
-export default Home;
